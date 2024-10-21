ee7f9c02131dea3c2aa6b1b7c0a9388f8e578baf
Justin Leung <justinleung074170@gmail.com>
2024-10-21 14:19:56 +0100

    Basic structure for RTS_Model class in model.py

---
+++ pytomofilt/model.py
@@ -0,0 +1,18 @@
+
+
+
+class RTS_Model(Model):
+
+    def __init__():
+
+    def from_file(filename):
+
+    def from_layers(filename):
+
+    def filter_from_file(self, filename):
+
+    def filter(self, model):
+    
+    def correlate(self, model):
+    
+    def write(self, filename):
