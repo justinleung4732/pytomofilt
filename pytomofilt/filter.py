e5a3e5ecc69e5037061ccda4628bb10666d79364
Justin Leung <justinleung074170@gmail.com>
2025-04-22 16:53:23 +0100

    Problem seems to work by changing eigvals.size+1 to eigvals.size in line 233. Not sure why the +1 was needed there before?

--- pytomofilt/filter.py
+++ pytomofilt/filter.py
@@ -230,7 +230,7 @@ def apply_filter_inner(x, eigvals, eigvecs, twts, damp):
     eta = eigvals[0] * damp
     # loop over the eigenvectors and values 
     # we read in
-    for i in numba.prange(eigvals.size+1):
+    for i in numba.prange(eigvals.size):
             
         w = ( (eigvals[i] / (eigvals[i] + eta)) * 
                np.sum(twtsinv[:] * x[:] * eigvecs[i,:]) )
