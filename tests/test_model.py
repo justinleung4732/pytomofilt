f3270af943dab2bf9bebc09435d131fee08d24ac
Justin Leung <justinleung074170@gmail.com>
2026-02-23 10:33:06 +0000

    Update test to check for radius rather than depth in RealLayer

--- tests/test_model.py
+++ tests/test_model.py
@@ -13,7 +13,8 @@ class TestDataclass(unittest.TestCase):
         # Build a model
         layers = []
         for depth in [10.0, 20.0, 30.0, 50.0, 440.0, 660.0, 1000.0, 2000.0, 2500.0]:
-            layers.append(mod.RealLayer(depth,
+            radius = 6371 - depth
+            layers.append(mod.RealLayer(radius,
                                         lats=[90.0, 0.0, 0.0, 0.0, 0.0, -90.0],
                                         lons=[0.0, 0.0, 90.0, 180.0, 270.0, 0.0], 
                                         vals=[10.0, 20.0, 20.0, 20.0, 20.0, 10.0]
@@ -21,7 +22,7 @@ class TestDataclass(unittest.TestCase):
             
         layer_model = mod.RealLayerModel(layers)
         self.assertIsInstance(layer_model.layers[1], mod.RealLayer)
-        self.assertEqual(layer_model.layers[1].depth, 20.0)
+        self.assertEqual(layer_model.layers[1].radius, 6351.0)
         self.assertEqual(layer_model.layers[1].lats,
                          [90.0, 0.0, 0.0, 0.0, 0.0, -90.0])
         self.assertEqual(layer_model.layers[1].lons,
@@ -158,7 +159,8 @@ class TestReparam(unittest.TestCase):
 
         layers = []
         for depth in [10.0, 20.0, 30.0, 2000.0, 2500.0]:
-            layers.append(mod.RealLayer(depth,
+            radius = 6371 - depth
+            layers.append(mod.RealLayer(radius,
                                         lats=[90.0, 0.0, 0.0, 0.0, 0.0, -90.0],
                                         lons=[0.0, 0.0, 90.0, 180.0, 270.0, 0.0], 
                                         vals=[10.0, 20.0, 20.0, 20.0, 20.0, 10.0]
